88a20eb950b973d8a677eaf4411bb3796cc57492
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-15 13:10:10 +0700

    config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: path.join(__dirname, 'src', 'index.tsx'),
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		port: 3000,
+	},
+	target: 'web',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.jsx'],
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'build'),
+	},
+};
