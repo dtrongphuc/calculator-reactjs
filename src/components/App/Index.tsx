9d7d8dc7614c6bae88885a86c8292984793eba5d
dtrongphuc <dtrongphucdv@gmail.com>
2020-09-17 14:50:30 -0700

    fix ui

--- src/components/App/Index.tsx
+++ src/components/App/Index.tsx
@@ -6,19 +6,21 @@ import ButtonFunction from 'Components/ButtonFunction/Index';
 import ButtonNumber from 'Components/ButtonNumber/Index';
 import styles from './Index.module.css';
 
-const App: React.FunctionComponent = () => (
-	<div className={`${styles.app}`}>
-		<Output />
-		<div className={styles.wrapper}>
-			<div className={styles.left}>
-				<ButtonFunction />
-				<ButtonNumber />
-			</div>
-			<div className={styles.right}>
-				<ButtonOperator />
+const App: React.FunctionComponent = () => {
+	return (
+		<div className={`${styles.app}`}>
+			<Output />
+			<div className={styles.wrapper}>
+				<div className={styles.left}>
+					<ButtonFunction />
+					<ButtonNumber />
+				</div>
+				<div className={styles.right}>
+					<ButtonOperator />
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default App;
--- src/components/Output/Index.tsx
+++ src/components/Output/Index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import styles from './Index.module.css';
 
 const Output: React.FunctionComponent = () => {
-	return <div className={styles.result}></div>;
+	return (
+		<div className={styles.output}>
+			<span className={styles.number}>0</span>
+		</div>
+	);
 };
 
 export default Output;
